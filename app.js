e0c0cb0a3ec88c72b16fef95c527562cf15a742d
Pawel Owiesek <Owiesek.p@gmail.com>
2026-06-24 18:02:02 +0200

    hamburger close on click

--- app.js
+++ app.js
@@ -58,7 +58,14 @@ ScrollReveal().reveal(".gallery img", {
 });
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-menu a");
 
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  });
 });
